643defa6a92cd5343e658e8c5acaf69e2a95ef03
Arthur <neiklotrr@gmail.com>
2023-08-31 22:47:34 +0300

    store items

---
+++ data/data.js
@@ -0,0 +1,33 @@
+import img1 from '../assets/images/prod1.png'
+import img2 from '../assets/images/prod2.png'
+import img3 from '../assets/images/prod3.png'
+
+
+export const storeItems = [
+    {
+        id: 1,
+        title: 'Футболка UZcotton мужская',
+        price: 522,
+        noDiscount: '1051 сом',
+        quantityRemaining: 'Осталось 2 шт.',
+        color: 'Белый',
+        size: '58',
+        img: img1,
+        address: 'Коледино WB',
+        producer: 'OOO Мегапрофстиль',
+        innitialQuanitity: 1,
+    },
+    {
+        id: 2,
+        title: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
+        price: 522,
+        noDiscount: '1051 сом',
+        quantityRemaining: 'Осталось 2 шт.',
+        color: 'Белый',
+        size: '58',
+        img: img1,
+        address: 'Коледино WB',
+        producer: 'OOO Мегапрофстиль',
+        innitialQuanitity: 1,
+    }
+]
