c15eace7ebdb08f1ee07f8698d64e68df6c4644e
Arthur <neiklotrr@gmail.com>
2023-09-05 16:46:11 +0300

    toggle arrows

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import { generateProducts, hideProducts, showProducts } from './components/CartItem.js';
 import { generateMissing, hideMissing, showMissing } from './components/MissingItem.js';
 import { decrement, increment } from './lib/counterOperations.js';
-import { updateTotals } from './lib/updateTotals.js';
+import { updateTotals, updateTotalBtn } from './lib/updateTotals.js';
 import { validateInputs } from './lib/formValidation.js';
+import { toggleCheckbox, toggleArrow } from './lib/checkboxToggle.js';
 
 const attachEventListeners = () => {
     const decrementBtns = document.querySelectorAll('.counter button:first-child');
@@ -10,40 +11,45 @@ const attachEventListeners = () => {
     const toggleCartBtn = document.querySelector('#toggle-cart');
     const toggleMissingBtn = document.querySelector('#toggle-missing')
     const orderBtn = document.querySelector('#order');
+    const paymentCheckbox = document.querySelector('#checkbox-instantpay');
 
     toggleCartBtn.addEventListener('click', (e) => {
         if (toggleCartBtn.value === 'open') {
             hideProducts();
-            toggleCartBtn.value = 'closed';
+            toggleArrow(toggleCartBtn);
         } else {
             showProducts();
-            toggleCartBtn.value = 'open';
+            toggleArrow(toggleCartBtn);
         }
     })
 
     toggleMissingBtn.addEventListener('click', (e) => {
         if (toggleMissingBtn.value === 'open') {
             hideMissing();
-            toggleMissingBtn.value = 'closed';
+            toggleArrow(toggleMissingBtn)
         } else {
             showMissing();
-            toggleMissingBtn.value = 'open';
+            toggleArrow(toggleMissingBtn)
         }
     })
 
     decrementBtns.forEach((btn) => {
         btn.addEventListener('click', (e) => {
             const id = btn.dataset.id;
+            const isBtnToggled = paymentCheckbox.value === 'on';
             decrement(id);
             updateTotals();
+            updateTotalBtn(isBtnToggled);
         })
     });
 
     incrementBtns.forEach((btn) => {
         btn.addEventListener('click', (e) => {
             const id = btn.dataset.id;
+            const isBtnToggled = paymentCheckbox.value === 'on';
             increment(id);
             updateTotals();
+            updateTotalBtn(isBtnToggled);
         })
     })
 
@@ -51,6 +57,10 @@ const attachEventListeners = () => {
         validateInputs();
     })
 
+    paymentCheckbox.addEventListener('click', (e) => {
+        const toggle = toggleCheckbox(paymentCheckbox);
+        updateTotalBtn(toggle);
+    })
 }
 
 generateProducts()
